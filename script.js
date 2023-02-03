064d0a63bc02cd59f86893e9ae92497cd9108fc0
oliverga <hi@oliverga.com>
2023-02-03 12:46:23 +0100

    split lines function and added more styling

--- script.js
+++ script.js
@@ -1,19 +1,35 @@
-const id = 1651;
+const id = 30183;
 const url = `https://kea-alt-del.dk/t7/api/products/${id}`;
+
 
 function getProduct() {
   fetch(url)
     .then((res) => res.json())
-    .then((data) => showProduct(data));
+    .then((data) => showProduct(data))
+    .then((data) => splitLines(data))
 }
 
 function showProduct(product) {
-document.querySelector(".product_name").textContent = product.productdisplayname;
-document.querySelector(".product_price").textContent = product.price;
-document.querySelector(".product_image").src = `https://kea-alt-del.dk/t7/images/webp/640/${id}.webp`;
-document.querySelector(".product_image").alt = product.productdisplayname;
-document.querySelector(".product_description").innerHTML = product.description;
-console.log(product)
+  console.log(product)
+  document.querySelector(".product_name").textContent = product.productdisplayname;
+  document.querySelector(".product_price").textContent = product.price;
+  document.querySelector(".product_img").src = `https://kea-alt-del.dk/t7/images/webp/640/${id}.webp`;
+  document.querySelector(".product_img").alt = product.productdisplayname;
+  document.querySelector(".brand_name").textContent = product.brandname;
+  document.querySelector(".breadcrumb_category").textContent = product.subcategory;
+  return product;
+}
+
+function splitLines(product) {
+  const paragraph = product.description;
+  const split = paragraph.split("<br />");
+
+  split.forEach((line) => {
+    const p = document.createElement("p");
+    p.innerHTML = line;
+    document.querySelector(".product_description").appendChild(p);
+    }
+  );
 }
 
 getProduct();
