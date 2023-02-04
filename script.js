37f4d98205b98cae037da00d62d4770ab619c965
oliverga <hi@oliverga.com>
2023-02-04 10:21:50 +0100

    fixed category page

--- script.js
+++ script.js
@@ -1,8 +1,118 @@
-let id = 12872;
-const url = `https://kea-alt-del.dk/t7/api/products/${id}`;
+const categoryTemplate = document.querySelector("#category_template");
+const subcategoryTemplate = document.querySelector("#subcategory_template");
+const categoriesUrl = `https://kea-alt-del.dk/t7/api/categories`;
+const subcategoriesUrl = `https://kea-alt-del.dk/t7/api/subcategories?category=`;
+
+let apiUrl = ``;
+// create categories object with subcategories array
+let categories = {};
+
+
+function getKategorier() {
+  fetch(categoriesUrl)
+  .then((res) => res.json())
+  .then((data) => {
+    data.forEach((category) => {
+      // console.log(category);
+      // create categories object with subcategories array
+      const safeCategory = category.category.replace(/\s+/g, '-');
+      categories[safeCategory] = [];
+      const categoryClone = categoryTemplate.content.cloneNode(true);
+      categoryClone.querySelector(".category_heading").innerHTML = category.category;
+      categoryClone.querySelector(".view_all").href = `produktliste.html?category=${category.category}`;
+      categoryClone.querySelector(".category_flex").id = safeCategory;
+      document.querySelector("#category_container").appendChild(categoryClone);
+
+      fetch(subcategoriesUrl + category.category)
+      .then((res) => res.json())
+      .then((subdata) => {
+        subdata.forEach((subcategory) => {
+          categories[safeCategory].push(subcategory.subcategory);
+        });
+        console.log(categories);
+        addSubcategories();
+      });
+
+    });
+  });
+}
+
+function addSubcategories() {
+  for (let category in categories) {
+    let categoryDiv = document.querySelector(`#${category}`);
+    console.log(categoryDiv)
+    categories[category].forEach(subcategory => {
+      if (!categoryDiv.querySelector(`a[href="produktliste.html?subcategory=${subcategory}"]`)) {
+        const subcategoryClone = subcategoryTemplate.content.cloneNode(true);
+        subcategoryClone.querySelector(".subcategory_link").innerHTML = subcategory;
+        subcategoryClone.querySelector(".subcategory_link").href = `produktliste.html?subcategory=${subcategory}`;
+        categoryDiv.appendChild(subcategoryClone);
+      }
+    });
+  }
+}
+
+
+// function addSubcategories() {
+//   for (let category in categories) {
+//     console.log(category);
+//     let categoryDiv = document.querySelector(`#${category}`);
+//     categories[category].forEach((subcategory) => {
+//       const subcategoryClone = subcategoryTemplate.content.cloneNode(true);
+//       subcategoryClone.querySelector(".subcategory_link").innerHTML = subcategory;
+//       subcategoryClone.querySelector(".subcategory_link").href = `produktliste.html?subcategory=${subcategory}`;
+//       categoryDiv.appendChild(subcategoryClone);
+//     });
+//   }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+const productCardTemplate = document.querySelector("#product_card_template");
+const productGrid = document.querySelector("#product_grid");
+
+const productsUrl = `https://kea-alt-del.dk/t7/api/products?limit=50`;
+
+function getProduktliste() {
+    fetch(productsUrl)
+    .then((res) => res.json())
+    .then((data) => {
+        data.forEach((product) => {
+            console.log(product);
+            const productClone = productCardTemplate.content.cloneNode(true);
+            productClone.querySelector(".product_img").src = `https://kea-alt-del.dk/t7/images/webp/640/${product.id}.webp`;
+            productClone.querySelector(".product_name").textContent = product.productdisplayname;
+            productClone.querySelector(".product_price").textContent = product.price;
+            productGrid.appendChild(productClone);
+        });
+    });
+}
+
+
+
+
+
+
+
+
+
+
+
+
+let id = 12538;
+let productIdUrl = `https://kea-alt-del.dk/t7/api/products/${id}`;
 
 function getProduct() {
-  fetch(url)
+  fetch(productIdUrl)
     .then((res) => res.json())
     .then((data) => showProduct(data))
     .then((data) => splitLines(data))
@@ -31,4 +141,14 @@ function splitLines(product) {
   );
 }
 
-getProduct();
+window.onload = function() {
+  if(window.location.href.includes("produktliste.html")) {
+      getProduktliste();
+  }
+  else if(window.location.href.includes("kategori.html")) {
+      getKategorier();
+  }
+  else if(window.location.href.includes("produkt.html")) {
+      getProduct();
+  }
+}
